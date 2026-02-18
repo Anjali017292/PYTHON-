39c10ae16afe6c41ba126043c67be85a27c9ee0b
Anjali017292 <jattianjali1703@gmail.com>
2026-02-18 21:54:12 +0530

    this file contain conditional statement (triangle program)

--- labwork_18feb/triangle.py
+++ labwork_18feb/triangle.py
@@ -1,20 +1,22 @@
 #program to verify whether three angles can form a triangle or not 
 #input of three angles
 angle1=float(input("Enter first angle"))
-if(angle1<=0):
+if angle1<=0:
      print("Invalid angle")
      exit()
 else:
     angle2=float(input("Enter second angle"))
-    if(angle2<=0):
+    if angle2<=0:
         print("Invalid angle")
         exit()
     else:
-        angle3=int(input("Enter third angle"))
-        print("Invalid angle")
-        exit()
-if (angle1+angle2+angle3)==180:
-    print("Valid Triangle")
-else:
-    print("Invalid Triangle")
+        angle3=float(input("Enter third angle"))
+        if(angle3<=0):
+            print("Invalid angle")
+            exit()
+        else:
+            if (angle1+angle2+angle3)==180:
+                print("Valid Triangle")
+            else:
+                print("Invalid Triangle")
     
