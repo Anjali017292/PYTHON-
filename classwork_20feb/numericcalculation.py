e402ec009c093017001774ba5d69229b3a73d8f7
Anjali017292 <jattianjali1703@gmail.com>
2026-02-20 20:53:19 +0530

    this file contain numeric calculation module program with output

---
+++ classwork_20feb/numericcalculation.py
@@ -0,0 +1,28 @@
+def displayaddition(a,b):
+    print("Addition of",a,"and",b,"is:",(a+b))
+def calculationaddition(a,b):
+    return (a+b)
+    
+def displaysubtraction(a,b):
+    print("Addition of",a,"and",b,"is:",(a-b))
+def calculationsubtraction(a,b):
+    return (a+b)
+    
+def displayproduct(a,b):
+    print("Product of",a,"and",b,"is:",(a*b))
+def calculationproduct(a,b):
+    return (a*b)
+
+def displayDivision(a,b):
+    if b==0:
+        print("Division is not allowed")
+    else:
+        print("Product of",a,"and",b,"is:",(a/b))
+def calculationproduct(a,b):
+    return (a/b)
+    
+def calculateremainder(a,b):
+    if b==0:
+        return None
+    return a%b
+        
