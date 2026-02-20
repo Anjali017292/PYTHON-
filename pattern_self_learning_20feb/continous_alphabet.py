bbdbce7bde3a775b3326db28a8cba0e222a338ce
Anjali017292 <jattianjali1703@gmail.com>
2026-02-20 21:58:34 +0530

    this file contain contignous alphabet program with output

---
+++ pattern_self_learning_20feb/continous_alphabet.py
@@ -0,0 +1,15 @@
+# Continuous Alphabet Pattern
+ch = 65
+for i in range(1,6):
+    for j in range(i):
+        print(chr(ch), end=" ")
+        ch += 1
+    print()
+    
+'''output:
+A
+B C
+D E F
+G H I J
+K L M N O
+'''
