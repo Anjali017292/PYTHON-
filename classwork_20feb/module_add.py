4c8f08e7f8cf1ae642bb19514c5623852a3c337e
Anjali017292 <jattianjali1703@gmail.com>
2026-02-20 20:54:34 +0530

    this file contain add module program with output

---
+++ classwork_20feb/module_add.py
@@ -0,0 +1,13 @@
+#to import the module
+import numericcalculation
+
+
+#To display the addition of two numbers
+numericcalculation.displayaddition(30,89)
+
+#To display the subtraction of two numbers
+numericcalculation.displaysubtraction(80,40)
+
+'''output:addition of 30 and 89 is: 119
+subtraction of 80 and 40 is: 40
+'''
