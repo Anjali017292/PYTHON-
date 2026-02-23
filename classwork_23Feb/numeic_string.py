0819dd5b0554ec402424373c31436bddaa36000e
Anjali017292 <jattianjali1703@gmail.com>
2026-02-23 16:42:58 +0530

    this file contain numeric string program and output

---
+++ classwork_23Feb/numeic_string.py
@@ -0,0 +1,18 @@
+#create a string 1
+sentence="Hello world"
+print(sentence.isalnum())
+
+#create a string 2
+str3='hello 123'
+print(str3.isalnum())
+
+#create a string 3
+str4="123"
+print(str4.isalnum())
+
+
+'''#output
+False
+False
+True'''
+
