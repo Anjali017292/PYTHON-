d58b8dbde04f8ec0aeb4e0e9c3ea193a3e339740
Anjali017292 <jattianjali1703@gmail.com>
2026-02-24 15:45:21 +0530

    this file contain string digits check  program and output

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style_24feb/string_digits_check98.py
@@ -0,0 +1,11 @@
+# Check if a string contains only digits
+original_string = "12345"
+is_all_digits = original_string.isdigit()
+print(f"Original string: {original_string}")
+print(f"Is all digits? {is_all_digits}")
+
+'''
+Output:
+Original string: 12345
+Is all digits? True
+'''
