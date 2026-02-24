9063bfa32a95372d30a6e98ea0db301a6c913c07
Anjali017292 <jattianjali1703@gmail.com>
2026-02-24 12:02:41 +0530

    This file contain print natural number from 1 to n program and output

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style_24feb/print_1toN_21.py
@@ -0,0 +1,19 @@
+# Print numbers from 1 to N using while loop
+
+n = int(input("Enter a number: "))
+#using while loop to print numbers from 1 to N
+i = 1
+while i <= n:
+    print(i)
+    i += 1
+    
+    
+'''#output:
+    Enter a number: 5
+    1
+    2
+    3
+    4
+    5'''
+    
+    
