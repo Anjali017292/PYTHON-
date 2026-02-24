c1e8673600829a55cc9d57d2393b3e16164918b5
Anjali017292 <jattianjali1703@gmail.com>
2026-02-24 10:22:33 +0530

    This file contain program to check number is negative , positive or zero with output

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style_24feb/check_number_sign2.py
@@ -0,0 +1,14 @@
+# Read input from user
+num = float(input("Enter a number: "))
+
+# Check whether the number is positive, negative, or zero
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+    
+'''# Output:
+Enter a number: 5
+The number is positive.'''
