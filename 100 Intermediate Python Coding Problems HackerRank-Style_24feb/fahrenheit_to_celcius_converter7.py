b9cd8477b4bf72edb73251e5b3424c52fe920a1c
Anjali017292 <jattianjali1703@gmail.com>
2026-02-24 10:58:07 +0530

    This file contain fahrenheit to celcius  converter program and output

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style_24feb/fahrenheit_to_celcius_converter7.py
@@ -0,0 +1,9 @@
+#input temperature in Celsius from user
+celsius = float(input("Enter temperature in Celsius: "))
+# Convert Celsius to Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+# Output the result
+print("Temperature in Fahrenheit:", round(fahrenheit, 2))
+'''# Output:
+Enter temperature in Celsius: 25
+Temperature in Fahrenheit: 77.0'''
