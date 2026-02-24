bbb2ddcd1aa68657ee47bf40ed87cdcef03dbaef
Anjali017292 <jattianjali1703@gmail.com>
2026-02-24 11:15:43 +0530

    this file contain leap year checker  program and output

---
+++ 100 Intermediate Python Coding Problems HackerRank-Style_24feb/leap_year_checker11.py
@@ -0,0 +1,14 @@
+# Leap Year Checker
+year = int(input("Enter a year: "))
+
+# Check if the year is a leap year
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
+    
+'''#output
+Enter a year: 2020
+2020 is a leap year.
+Enter a year: 2021
+2021 is not a leap year.'''
