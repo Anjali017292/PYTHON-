71df13dc21f72d0ef5c41e985bfd966ae0773cda
Anjali017292 <jattianjali1703@gmail.com>
2026-03-13 18:28:59 +0530

    this file contain find mean of an array program and output

---
+++ classwork_13_Mar/mean_numpy.py
@@ -0,0 +1,26 @@
+import numpy as np
+#array
+array1=np.array([32,56,78,90,56,45,89,12])
+print("First array :")
+print(array1)
+print("---------------")
+#mean
+print("Mean is:")
+print(np.mean(array1))
+print("---------------")
+#standard deviation
+print("Standard deviation is:")
+print(np.std(array1))
+print("---------------")
+
+'''output
+First array :
+[32 56 78 90 56 45 89 12]
+---------------
+Mean is:
+57.25
+---------------
+Standard deviation is:
+25.85899263312475
+---------------
+'''
