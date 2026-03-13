375dbc1e18b0cfe62ed6888c39342e7334e77fb0
Anjali017292 <jattianjali1703@gmail.com>
2026-03-13 18:29:43 +0530

    this file contain ones numpy program and output

---
+++ classwork_13_Mar/ones_numpy.py
@@ -0,0 +1,37 @@
+import numpy as np
+#creating ones array
+array1=np.ones((3,4))
+print("Ones array of order (3,4) :")
+print(array1)
+print("------------------")
+#Ones array
+array2=np.ones((5,3),dtype=int)
+print("Ones array of order (5,3) :")
+print(array2)
+print("------------------")
+#Ones array
+array3=np.ones((4,3),dtype=int)
+print("Ones array of order (4,3) :")
+print(array3)
+print("------------------")
+
+'''output
+Ones array of order (3,4) :
+[[1. 1. 1. 1.]
+ [1. 1. 1. 1.]
+ [1. 1. 1. 1.]]
+------------------
+Ones array of order (5,3) :
+[[1 1 1]
+ [1 1 1]
+ [1 1 1]
+ [1 1 1]
+ [1 1 1]]
+------------------
+Ones array of order (4,3) :
+[[1 1 1]
+ [1 1 1]
+ [1 1 1]
+ [1 1 1]]
+------------------
+'''
