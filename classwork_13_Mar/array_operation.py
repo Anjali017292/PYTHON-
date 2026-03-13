89728b0f7a8d0f684f47ef267a8483c9b6ded3eb
Anjali017292 <jattianjali1703@gmail.com>
2026-03-13 18:28:17 +0530

    this file contain array operations and use it program and output

---
+++ classwork_13_Mar/array_operation.py
@@ -0,0 +1,25 @@
+#creating an array of five elements
+import numpy as np
+number1=np.array([2,3,4,5,6])
+print("Arrary is:")
+print(number1)
+print("----------------")
+print("----------------")
+#multipy by 10
+print("Multiply by 10:")
+print(number1*10)
+print("----------------")
+print("----------------")
+
+'''output
+Arrary is:
+[2 3 4 5 6]
+----------------
+----------------
+Multiply by 10:
+[20 30 40 50 60]
+----------------
+----------------
+'''
+
+
