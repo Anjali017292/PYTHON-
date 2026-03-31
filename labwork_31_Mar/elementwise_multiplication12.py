fcacf3b4fa4b0c6eae239580c1a610ae1233491e
Anjali017292 <jattianjali1703@gmail.com>
2026-03-31 10:58:00 +0530

    This file contain Element wise multiplication  program and their output.

---
+++ labwork_31_Mar/elementwise_multiplication12.py
@@ -0,0 +1,15 @@
+import numpy as np   # import numpy
+
+# create two arrays
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# multiply elements at same position
+result = a * b
+
+# print result
+print("Result:", result)
+
+'''Output:
+Result: [ 4 10 18]
+'''
